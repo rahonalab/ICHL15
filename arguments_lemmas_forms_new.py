37a3057e945f11267a179c2513c82d660cb4ab1a
levshina <natalia.levshina@mpi.nl>
2022-06-10 15:46:34 +0200

    Add files via upload
    I've fixed some issues in these new versions. The code should be working with UD conllu files. I wasn't able to test it on CIEP data, that's why I didn't make changes in the CIEP scripts. The changes are as follows:
    1. arguments_lemmas_forms_new.py:
    1.1. A comment about languages with cases: sv, da and no can be excluded, in my view.
    1.2. findArguments(): Lemmas now include morphological features, except for case. This is done to solve the problem with number and definiteness marking, which can interfere with case forms overlap.
    1.3. cleanArguments(): A comment about Welsh: please check!
    1.4. cleanArguments(); Added "al" to Spanish as a possible object marker. 
     
    
    2. lemma_overlap_new.py:
    count_overlap_lemmas(): removed the condition for nouns only, which interfered. I think it's not necessary.

---
+++ arguments_lemmas_forms_new.py
@@ -0,0 +1,271 @@
+#!/usr/bin/env python3
+
+import glob
+from collections import OrderedDict
+from collections import defaultdict
+import re
+from random import sample 
+#The following dictionary should reflect source data directory: all the languages from our sample
+languages_ciep = ["bg","br","cs","cy","da","de","el","en","es","fa","fr","ga","hbs","hi","hy","it","kmr","la","lt","lv","nl","no","pl","pt","ro","ru","sk","sv","uk","ur"]
+#We include languages with morphological and syntactic (adposition) case marking
+languages_with_cases = languages_with_cases = ["cs",
+"cy",
+"da",
+"de",
+"el",
+"es",
+"fa",
+"fr",
+"hbs",
+"hi",
+"hy",
+"it",
+"kmr",
+"la",
+"lt",
+"lv",
+"no",
+"pl",
+"pt",
+"ro",
+"ru",
+"sk",
+"sv",
+"uk",
+"ur"]
+
+#!!!Natalia: not sure da, no and sv have cases, or have they?
+
+def findArguments(compounds = False):
+    language_list = languages_with_cases
+    args = ["nsubj", "obj", "iobj", "obl"]
+    for language in language_list:
+        print (language)
+        files = glob.glob("D:/Corpora/ud-treebanks-v2.7/UD_Swedish-Talbanken/*.conllu")
+        if compounds == False:
+            outfilename = "arguments_" + language + "_new.txt"
+        else:
+            outfilename = "arguments_" + language + "_compounds_new.txt"
+        outfile = open(outfilename, "wb")
+        for filename in files:
+            print (filename)
+            file = open(filename, "rb")
+            corpus = file.read()
+            corpus = str(corpus, "UTF-8", errors = "ignore")
+            file.close()
+            sentences = corpus.split("\n\n") # I had some problems with separators because the corpora had been created inconsistently. Please check this.
+            if len(sentences) == 1:
+                sentences = corpus.split("\n\r")
+            print (len(sentences))
+            for sentence in sentences:
+                lines = sentence.strip().split("\n")
+                line_tuples = []
+                for line in lines:
+                    linelist = line.strip().split("\t")
+                    if len(linelist) > 7:
+                        dep = linelist[7]
+                        dep_original = linelist[7]
+                        head_id = linelist[6] 
+                        if ":" in dep:
+                            dep = dep.split(":")[0]
+                        if dep == "conj":
+                            dep, head_id = coordination(head_id, lines)
+                        if dep in args:
+                            POS = linelist[3]
+                            if POS in ["NOUN", "PROPN", "VERB", "ADJ", "NUM", "SYM"]:     
+                                token_id = linelist[0]
+                                lemma = linelist[2].lower()
+                                if lemma == "_":
+                                    lemma = linelist[1].lower() 
+                                if compounds == True:
+                                    lemma = checkCompounds(lemma, token_id, lines)
+                                morphology = linelist[5]
+                                if morphology != "_":
+                                    morph_features = morphology.split("|")
+                                    morphology_output = "|"
+                                    for feature in morph_features:
+                                        if "Case" not in feature:
+                                            morphology_output = morphology_output + "|" + feature
+                                    if len(morph_features) > 0:
+                                        lemma = lemma + morphology_output
+                                if dep == "nsubj":                            
+                                    if checkTrans(head_id, lines):
+                                        dep = "nsubj_tr"
+                                    else:   
+                                        dep = "nsubj_intr" 
+                                wordform = linelist[1].lower()
+                                wordform = check_adposition(wordform, token_id, lines, language)                             
+                                out = lemma + "\t" + POS + "\t" + dep + "\t" + dep_original + "\t" + wordform + "\n"
+                                outfile.write(bytes(out, "UTF-8"))
+        outfile.close()
+
+# we won't need this function. Just added for completeness. The results based on compounds and bare nouns are similar.
+def checkCompounds(lemma, token_id, lines):
+    compound_dict = {int(token_id) : lemma}
+    compound_deps = ["compound", "fixed", "flat"]
+    for line in lines:
+        linelist = line.split("\t")
+        if len(linelist) == 10:
+            dep = linelist[7]
+            if ":" in dep:
+                dep = dep.split(":")[0]
+            if dep in compound_deps:
+                head_id = linelist[6]
+                if head_id == token_id:
+                    new_lemma = linelist[2].lower()
+                    if new_lemma == "_":
+                        new_lemma = linelist[1].lower()
+                    new_token_id = linelist[0]    
+                    compound_dict[int(new_token_id)] = new_lemma    
+                    for line1 in lines:
+                        linelist1 = line1.split("\t")
+                        if len(linelist1) == 10:
+                            dep1 = linelist1[7]
+                            if ":" in dep1:
+                                dep1 = dep1.split(":")[0]
+                            if dep1 in compound_deps:
+                                head_id1 = linelist1[6]
+                                if head_id1 == new_token_id:
+                                    new_new_lemma = linelist1[2].lower()
+                                    if new_new_lemma == "_":
+                                        new_new_lemma = linelist1[1].lower()
+                                        new_new_token_id = linelist1[0]    
+                                        compound_dict[int(new_new_token_id)] = new_new_lemma     
+    ordered = OrderedDict(sorted(compound_dict.items()))
+    compound_lemma = ""
+    for key in ordered.keys():
+        compound_lemma = compound_lemma + "_" + ordered[key]
+    compound_lemma = compound_lemma.strip("_")
+    return compound_lemma
+
+def strip_punct(word): 
+    clean_lemma = re.sub('[^\w]','', word, re.UNICODE)
+    return clean_lemma
+
+def coordination(head_id, lines): 
+    head_dep = "NA"
+    head_id1 = "NA"
+    for line in lines:
+        linelist = line.split("\t")
+        if len(linelist) == 10:
+            token_id = linelist[0]
+            if token_id == head_id:
+                head_dep = linelist[7]
+                head_id1 = linelist[6]
+    return head_dep, head_id1    
+    
+    
+def checkTrans(head_id, lines): 
+    trans = False
+    for line in lines:
+        linelist = line.split("\t")
+        if len(linelist) == 10:
+            dep = linelist[7]
+            if ":" in dep:
+                dep = dep.split(":")[0]
+            if dep == "obj":
+                head_id_new = linelist[6]
+                if head_id_new == head_id:
+                    trans = True
+    return trans  
+
+def check_adposition(wordform, token_id, lines, language):
+    wordform_full = wordform
+    for line in lines:
+        linelist = line.split("\t")
+        if len(linelist) == 10:
+            head_id = linelist[6]
+            if head_id == token_id:
+                dep = linelist[7]
+                if ":" in dep:
+                    dep = dep.split(":")[0]
+                if dep == "case": 
+                    adpos = linelist[1].lower()
+                    if language in ["es", "pt"]:
+                        if adpos == "a":
+                            wordform_full = wordform_full + "_" + adpos
+                    else:
+                        wordform_full = wordform_full + "_" + adpos #because there were many PPs that were erroneously treated as obj
+    return wordform_full
+
+#An additional round of clearning is necessary. I also think there should be more 
+
+def cleanArguments(compounds = False):
+    language_list = languages_with_cases
+    for language in language_list:
+        print (language)
+        if compounds == False: 
+            filename = "arguments_" + language + "_new.txt"
+            outfilename = "arguments_" + language + "_clean.txt"
+            #filename = target + "/" + language + "_forms.txt"
+            #outfilename = target + "/" + language + "_forms_clean.txt"
+        else:
+            filename = "arguments_" + language + "_compounds_new.txt"
+            outfilename = "arguments_" + language + "_compounds_clean.txt"
+        outfile = open(outfilename, "wb")
+        file = open(filename, "rb")
+        lines = file.readlines()
+        file.close()
+        print (len(lines))
+        for line in lines:
+            line_UTF = str(line, "UTF-8", errors = "ignore")
+            linelist = line_UTF.strip().split("\t")
+            if len(linelist) == 5:
+                POS = linelist[1]
+                dep = linelist[2]
+                wordform = linelist[4]
+                dep_detailed = linelist[3]
+                if dep == "nsubj_tr": #please double-check that
+                #if dep == "nsubj":
+                    coreArg = True
+                    if dep_detailed == "nsubj:pass":
+                        coreArg = False
+                    elif "_" in wordform: #no adpositions by default are allowed for subjects, except for Hindi, Japanese and Korean. We might need to extend that.
+                        if len(wordform.split("_")) > 2:
+                            coreArg = False
+                        casemarker = wordform.split("_")[1]
+                        if language in ["hi", "hin"]:
+                            if casemarker!= u"ने":
+                                coreArg = False 
+                        else:
+                            coreArg = False 
+                    if coreArg:
+                        outfile.write(line)                       
+                elif dep == "obj":
+                    coreArg = True
+                    if "_" in wordform: 
+                        if len(wordform.split("_")) > 2:
+                            coreArg = False
+                        casemarker = wordform.split("_")[1]
+                        if language in ["sp", "pt", "spa", "por", "es"]: 
+                            if casemarker not in ["a", "al"]: #added "al"
+                                coreArg = False
+                        elif language in ["it", "ita"]:
+                            if casemarker not in ["di", "d'", "d’"]:
+                                coreArg = False
+                        elif language in ["ro", "ron"]:
+                            if casemarker != "pe":
+                                coreArg = False
+                        elif language in ["fra", "fr"]: #Partitive
+                            if casemarker not in ["de", "d'", "d’", "du", "des"]:
+                                coreArg = False
+                        elif language in ["fa", "pes"]:
+                            if casemarker != u"را":
+                                coreArg = False
+                        elif language in ["hi", "hin"]:
+                            if casemarker != u"को":
+                                coreArg = False
+                        elif language in ["ur", "urd"]:
+                            if casemarker not in [u"نے", u"کو"]:
+                                coreArg = False
+#!!!Natalia: this is the branch for object marking. Are you sure this should be here? I haven't been able to find this use of yn with transitive subjects.
+#Could you please double-check and uncomment or tranfer to the subject branch, if necessary?
+#                        elif language in ["cy", "cym"]: #subject marking, King 2003:298-299 
+#                            if casemarker != "yn":
+#                                coreArg= False
+                        else:
+                            coreArg = False 
+                    if coreArg:
+                        outfile.write(line)                           
+        outfile.close()
+
---
+++ lemma_overlap_new.py
@@ -0,0 +1,48 @@
+language_list = ["swe"]#please add the language list
+
+def count_overlap_lemmas():
+    outfilename = "lemmas_overlap.txt"
+    outfile = open(outfilename, "w")
+    for language in language_list:
+        print (language)
+        lemmas_overlap = 0
+        lemmas_total = 0
+        lemmas_overlap_weighted = 0
+        lemmas_total_weighted = 0
+        dict_nsubj = defaultdict(list)
+        dict_obj = defaultdict(list)
+        infilename = "arguments_" + language + "_clean.txt"#please modify it as you need
+        infile = open(infilename, "rb")
+        lines = infile.readlines()
+        for line in lines:
+            line = str(line, "UTF-8",errors = 'ignore')
+            linelist = line.strip().split("\t")           
+            try:
+                lemma = linelist[0].strip()
+                if lemma != "_" and len(lemma) > 0:
+                    lemmaPOS = lemma + "/" + linelist[1]                    
+                    if linelist[2] == "obj":
+                        dict_obj[lemmaPOS].append(linelist[4].strip())
+                    elif linelist[2] == "nsubj_tr":
+                        dict_nsubj[lemmaPOS].append(linelist[4].strip())
+            except:
+                print (line)
+        set_nsubj = set(dict_nsubj)
+        set_obj = set(dict_obj)
+        set_both = set_nsubj & set_obj 
+        for key in set_both:
+            list_obj = dict_obj[key]
+            list_nsubj = dict_nsubj[key]
+            freq = len(list_obj) + len(list_nsubj)
+            if freq  > 0:
+                obj_form = sample(list_obj, 1)
+                nsubj_form = sample(list_nsubj, 1)
+                if obj_form == nsubj_form:
+                    lemmas_overlap_weighted += 1*freq
+                    lemmas_overlap += 1
+                lemmas_total_weighted += 1*freq
+                lemmas_total += 1
+        out = language + "\t" + str(lemmas_overlap) + "\t" + str(lemmas_total) + "\t" + str(lemmas_overlap_weighted) + "\t" + str(lemmas_total_weighted) + "\n"
+        outfile.write(out)
+        print(out)
+    outfile.close()
